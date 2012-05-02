fd107175fd2301d2d65518301721411793a35ba3
Ikram Chraibi <ichraibi@epsi.fr>
2012-05-02 14:49:35 +0200

    modif de paiement css

--- LocationVideoWebApp/WebContent/paiement.jsp
+++ LocationVideoWebApp/WebContent/paiement.jsp
@@ -66,41 +66,42 @@ String monSupport = request.getParameter("support");
 	
 	<table>
 		<tr>
-			<td class="gauche">
-				<form name="paiement" action="location.html?id=<% out.print(maVideo.getId());%>" method="post">
+			<td class="gauche" width = 50%>
+				<form name="paiementCB" action="location.html?id=<% out.print(maVideo.getId());%>" method="post">
+				<input type="hidden" name="type" value="CB" />
 				<table>
-					<tr class ="image"><img src="STYLE/images/visacb.jpg" width=70px/></tr>
+					<tr><td align="center"><img src="STYLE/images/visacb.jpg" width=70px/></td></tr>
 					<tr><td class="cle"><label for="nom"><% out.print("Nom propriétaire de la carte :");%> </label></td></tr>
-					<tr><td  width =200><input type="text" id="nom" name="nom" /></td></tr>
+					<tr><td><input type="text" id="nom" name="nom" /></td></tr>
 					
 					<tr><td><label for="numCarte"><% out.print("N° de carte bancaire :");%> </label></td></tr>
-					<tr><td  width =200><input type="text" id="numCarte" name="numCarte" /></td></tr>
+					<tr><td><input type="text" id="numCarte" name="numCarte" /></td></tr>
 		
 					<tr><td class="cle"><label for="cle"><% out.print("Clé :");%> </label></td></tr>
-					<tr><td  width =200><input type="text" id="cle" name="cle" /></td></tr>
+					<tr><td><input type="text" id="cle" name="cle" /></td></tr>
 							
 					
-					<tr><td class="cle"><label for="date"><% out.print("Clé :");%> </label></td></tr>
-					<tr><td  width =200><input type="text" id="date" name="date" /></td></tr>
+					<tr><td class="cle"><label for="date"><% out.print("Date d'expiration :");%> </label></td></tr>
+					<tr><td><input type="text" id="date" name="date" /></td></tr>
 					
-					<tr><td class="label"><input type="submit" name="Valider" value="Valider" /></td></tr>
+					<tr align="center"><td class="label"><input type="submit" name="Valider" value="Valider" /></td></tr>
 				</table>
 			</form>
 			</td>
 			<td>
-				<form name="paiement" action="location.html?id=<% out.print(maVideo.getId());%>" method="post">
+				<form name="paiementCC" action="location.html?id=<% out.print(maVideo.getId());%>" method="post">
+				<input type="hidden" name="type" value="CC" />
 				<table>
-					<tr class ="image"><label>Carte Club "Poney Vidéos" : </label></tr>
-					
-					
-					<tr><td><label for="numCarte"><% out.print("N° de carte :");%> </label></td></tr>
-					<tr><td  width =200><input type="text" id="numCarte" name="numCarte" /></td></tr>
+					<tr><td align="center"><img src="STYLE/images/poney-card.png" width=70px/><td></tr>
+			
+					<tr><td><label for="numCarteC"><% out.print("N° de carte :");%> </label></td></tr>
+					<tr><td  width =200><input type="text" id="numCarteC" name="numCarteC" /></td></tr>
 		
 					<tr><td class="label"><label for="mdp"><% out.print("Mot de passe :");%> </label></td></tr>
 					<tr><td><input type="password" id="mdp" name="mdp" /></td></tr>
 							
 									
-					<tr><td class="label"><input type="submit" name="Valider" value="Valider" /></td></tr>
+					<tr align="center"><td class="label"><input type="submit" name="Valider" value="Valider" /></td></tr>
 				</table>
 			</form>
 				
--- LocationVideoWebApp/src/controller/LocationController.java
+++ LocationVideoWebApp/src/controller/LocationController.java
@@ -27,49 +27,75 @@ public class LocationController extends Controller {
 
 	public void process() throws ServletException, IOException
 	{
-		if (request.getParameter("numCarte")!=null && !request.getParameter("numCarte").isEmpty()
-				&& request.getParameter("cle")!=null && !request.getParameter("cle").isEmpty()
-				&& request.getParameter("nom")!=null && !request.getParameter("nom").isEmpty()
-				&& request.getParameter("date")!=null && !request.getParameter("date").isEmpty())
+		if (request.getParameter("type")!=null && !request.getParameter("type").isEmpty())
 		{
-			HttpSession session = request.getSession();
-			Utilisateur Uti = (Utilisateur)session.getAttribute("uti") ;
-			if(Uti != null)
-			{
-				Location location = new Location();
-				location.setDateLocation(new Date());
-				
-				GregorianCalendar calendar = new java.util.GregorianCalendar(); 
-				calendar.setTime( new Date() ); 
-				calendar.add (Calendar.DATE, 14);
-				
-				location.setDateLimiteRetour(calendar.getTime());
-	
-				
-				int idVideo = Integer.parseInt(request.getParameter("id"));
-				Video maVideo = LocVideoBean.getInstance().getVideo(idVideo);
-				
-				location.setVideo(maVideo);
-				location.setUtilisateur(Uti);
-				
-				LocVideoBean.getInstance().ajoutHistorique(location);
-				
-				request.setAttribute("location", maVideo.getNom());
-				
-				dispatch("acceuil.html");
+			if(request.getParameter("type").equals("CB"))
+			{	
+				if (request.getParameter("numCarte")!=null && !request.getParameter("numCarte").isEmpty()
+						&& request.getParameter("cle")!=null && !request.getParameter("cle").isEmpty()
+						&& request.getParameter("nom")!=null && !request.getParameter("nom").isEmpty()
+						&& request.getParameter("date")!=null && !request.getParameter("date").isEmpty())
+				{
+					ValiderLocation();
+				}
+				else
+				{
+					RedirectSiErreur();
+				}
 			}
-			else
+			else if(request.getParameter("type").equals("CC"))
 			{
-				dispatch("acceuil.html");
+				if (request.getParameter("numCarteC")!=null && !request.getParameter("numCarteC").isEmpty()
+						&& request.getParameter("mdp")!=null && !request.getParameter("mdp").isEmpty())
+				{
+					ValiderLocation();
+				}
 			}
 		}
 		else
 		{
-			int idVideo = Integer.parseInt(request.getParameter("id"));
-			request.setAttribute("id", idVideo);
-			request.setAttribute("erreur","true");
-			dispatch("paiement.jsp");
+			RedirectSiErreur();
 		}
 		
+	}
+
+	private void RedirectSiErreur() throws ServletException, IOException {
+		int idVideo = Integer.parseInt(request.getParameter("id"));
+		request.setAttribute("id", idVideo);
+		request.setAttribute("erreur","true");
+		dispatch("paiement.jsp");
+	}
+
+	private void ValiderLocation() throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Utilisateur Uti = (Utilisateur)session.getAttribute("uti") ;
+		if(Uti != null)
+		{
+			Location location = new Location();
+			location.setDateLocation(new Date());
+			
+			GregorianCalendar calendar = new java.util.GregorianCalendar(); 
+			calendar.setTime( new Date() ); 
+			calendar.add (Calendar.DATE, 14);
+			
+			location.setDateLimiteRetour(calendar.getTime());
+
+			
+			int idVideo = Integer.parseInt(request.getParameter("id"));
+			Video maVideo = LocVideoBean.getInstance().getVideo(idVideo);
+			
+			location.setVideo(maVideo);
+			location.setUtilisateur(Uti);
+			
+			LocVideoBean.getInstance().ajoutHistorique(location);
+			
+			request.setAttribute("location", maVideo.getNom());
+			
+			dispatch("acceuil.html");
+		}
+		else
+		{
+			dispatch("acceuil.html");
+		}
 	}
 }
